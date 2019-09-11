5d1479ee73e83e03bdfc6be8c291e50b6a6b4461
NiFLMadden <NiFLMadden@hotmail.com>
2019-09-11 22:25:41 +0100

    Update app.js with firebase info

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("./nifl-apps-firebase-adminsdk-y3hxl-206d236173.json");
+const serviceAccount = require("./nifl-app-firebase-adminsdk-85gtl-443c34f686.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
    credential: admin.credential.cert(serviceAccount),
-   databaseURL: "https://nifl-apps.firebaseio.com/"
+   databaseURL: "https://nifl-app.firebaseio.com/"
 });
 
 
